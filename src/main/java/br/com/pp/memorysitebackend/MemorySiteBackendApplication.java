de26cc44acbf01224b545ea0161646ac4225ce8b
JoaoVitor-QA <joaovitor.santos@actuar.com>
2025-04-16 19:08:33 -0300

    first commit

---
+++ src/main/java/br/com/pp/memorysitebackend/MemorySiteBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.pp.memorysitebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemorySiteBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemorySiteBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/pp/memorysitebackend/MemorySiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pp.memorysitebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemorySiteBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
